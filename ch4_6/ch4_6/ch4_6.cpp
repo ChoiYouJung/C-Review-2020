a2c35932af690dd8d39c69c3f068b24375b2e8e6
ChoiYouJung <53049928+ChoiYouJung@users.noreply.github.com>
2020-03-21 13:39:07 +0900

    Create ch4_6.cpp

---
+++ ch4_6/ch4_6/ch4_6.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "입력할 정수의 개수는?";
+
+	int n;
+	cin >> n; // 정수의 개수 입력
+	if (n <= 0) return 0;
+	int *p = new int[n];
+	if (!p) {
+		cout << "메오리를 할당할 수 없습니다.";
+		return 0;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << "번째 정수: ";
+		cin >> p[i];
+	}
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += p[i];
+	cout << "평균 = " << sum / n << endl;
+
+	delete [] p;
+	
+	cout << "second test" << endl;
+
+	char *pa = new char[5];
+
+	char *ppa = pa;
+
+	ppa[0] = 'a';
+
+	pa[1] = 'b';
+
+	cout << ppa[0] << endl;
+
+	cout << pa[1] << endl;
+
+	cout << ppa[1] << endl;
+
+	delete pa;
+}
